c86f6146712560827580cc0e7a33b53a65e11d44
MateoSacre <sacre.mateoo@gmail.com>
2022-03-18 16:20:02 +0100

    Base du site
    Crétation de la base du site avec Index.htm; module 'card' + style

---
+++ src/App.vue
@@ -0,0 +1,87 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/Card.vue';
+import stromoyen from "./assets/stromoyen.jpg"
+</script>
+
+<template>
+  <div class="row row-cols-1 row-cols-md-3 g-3 " style="display: flex; justify-content: center;  margin-left: 2%;margin-right: 2% ;">
+    <Card
+      cardTitle="Stromoyen"
+      :cardImg="stromoyen"
+      cardImgAlt="Photo de Storomoyen"
+      cardText="description complete"
+      cardType="Chanteur"
+      cardPrice="5,46"
+    />
+
+    <Card
+      cardTitle="Lorem"
+      :cardImg="stromoyen"
+      cardImgAlt="Photo de Storomoyen"
+      cardText="Lorem ipsum dolor sit ametnd ut erat eu ornare. Maecenas vel auctor nibh. Etiam imperdiet rutrum nibh aliquet suscipit. Cras nisl ipsum, aliquet a sapien in, gravida lacinia augue. Nunc et dui volutpat, dapibus arcu eu, convallis erat."
+      cardType="ipsum"
+      cardPrice="5,46"
+    />
+
+    <Card
+      cardTitle="Lorem"
+      :cardImg="stromoyen"
+      cardImgAlt="Photo de Storomoyen"
+      cardText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend ut erat eu ornare. Maecenas vel auctor nibh. Etiam imperdiet rutrum nibh aliquet suscipit. Cras nisl ipsum, aliquet a sapien in, gravida lacinia augue. Nunc et dui volutpat, dapibus arcu eu, convallis erat."
+      cardType="ipsum"
+      cardPrice="5,46"
+    />
+
+    <Card
+      cardTitle="Lorem"
+      :cardImg="stromoyen"
+      cardImgAlt="Photo de Storomoyen"
+      cardText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend ut erat eu ornare. Maecenas vel auctor nibh. Etiam imperdiet rutrum nibh aliquet suscipit. Cras nisl ipsum, aliquet a sapien in, gravida lacinia augue. Nunc et dui volutpat, dapibus arcu eu, convallis erat."
+      cardType="ipsum"
+      cardPrice="5,46"
+    />
+
+    <Card
+      cardTitle="Lorem"
+      :cardImg="stromoyen"
+      cardImgAlt="Photo de Storomoyen"
+      cardText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend ut erat eu ornare. Maecenas vel auctor nibh. Etiam imperdiet rutrum nibh aliquet suscipit. Cras nisl ipsum, aliquet a sapien in, gravida lacinia augue. Nunc et dui volutpat, dapibus arcu eu, convallis erat."
+      cardType="ipsum"
+      cardPrice="5,46"
+    />
+
+    <Card
+      cardTitle="Lorem"
+      :cardImg="stromoyen"
+      cardImgAlt="Photo de Storomoyen"
+      cardText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend ut erat eu ornare. Maecenas vel auctor nibh. Etiam imperdiet rutrum nibh aliquet suscipit. Cras nisl ipsum, aliquet a sapien in, gravida lacinia augue. Nunc et dui volutpat, dapibus arcu eu, convallis erat."
+      cardType="ipsum"
+      cardPrice="5,46"
+    />
+
+    <Card
+      cardTitle="Lorem"
+      :cardImg="stromoyen"
+      cardImgAlt="Photo de Storomoyen"
+      cardText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend ut erat eu ornare. Maecenas vel auctor nibh. Etiam imperdiet rutrum nibh aliquet suscipit. Cras nisl ipsum, aliquet a sapien in, gravida lacinia augue. Nunc et dui volutpat, dapibus arcu eu, convallis erat."
+      cardType="ipsum"
+      cardPrice="5,46"
+    />
+  </div>
+  
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+import {ref} from 'vue'
+
+defineProps({
+    cardTitle: String,
+    cardText: String,
+    cardImg: String,
+    cardImgAlt: String,
+    cardPrice: String,
+    cardType: String
+})
+
+</script>
+
+<template>
+<div class="card text-white bg-dark" style="width: 20rem; margin: 2%;" >
+    <img  class="card-img-top" :src="cardImg" alt={{cardImgAlt}} >
+    <div class="card-body ">
+        <h5 class="card-title">{{cardTitle}}</h5>
+        <p class="card-text">{{cardText}}</p>
+    </div>
+    <ul class="text-white bg-dark list-group list-group-flush ">
+        <li class="text-white bg-dark list-group-item">Type : {{cardType}}</li>
+        <li class="text-white bg-dark list-group-item">Prix : {{cardPrice}}€</li>
+    </ul>
+</div>
+</template>
+
+
