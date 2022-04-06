a956eb49abc0f40171c02754a8ff8714ec5248b4
MateoSacre <sacre.mateoo@gmail.com>
2022-04-06 11:35:52 +0200

    Ajout css

--- src/components/BigCard.vue
+++ src/components/BigCard.vue
@@ -17,17 +17,17 @@ export default {
 <template>
 
 
-    <div class="myarticlecard text_white bg-dark mb-3" style="width: 90%">
-        <img class="myarticleimg" :src="cardImg" :alt="cardImgAlt">
-         <div class="card-body ">
-            <h5 class="myarticletitle">{{cardTitle}}</h5>
-            <p class="myarticletext">{{cardText}}</p>
+    <div class="myarticlecard bg-dark mb-3" style="width: 90%; margin-top: 5%;">
+        <img class="myarticleimg " :src="cardImg" :alt="cardImgAlt">
+        <div class="mycard">
+            <h5 class="myarticletitle text-white">{{cardTitle}}</h5>
+            <p class="myarticletext text-white">{{cardText}}</p>
             <ul class="text-white bg-dark list-group list-group-flush ">
                 <li class="text-white bg-dark list-group-item">Type : {{cardType}}</li>
                 <li class="text-white bg-dark list-group-item">Prix : {{cardPrice}}€</li>
             </ul>
+            <button class="btn btn-danger" style="margin: 20px; width: 95%;">Supprimer</button>
         </div>
-        
     </div>
 </template>
 
